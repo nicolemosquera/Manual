908e350ba4707577bfc6e480846778395eac82b8
Alinson Mosquera <mosqueraalinson0301@gmail.com>
2026-06-02 08:25:09 -0500

    hola

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://nicolemosquera.github.io/Manual/',
+  base: '/Manual/',
   vite: {
     plugins: [tailwindcss()]
   }
